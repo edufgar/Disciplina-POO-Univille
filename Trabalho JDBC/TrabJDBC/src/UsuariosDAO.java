a8a9b646349ad4b9766a9c54d9a614ec2b84467a
edufgar <35237258+edufgar@users.noreply.github.com>
2019-10-02 22:21:14 -0300

    Update UsuariosDAO.java

--- Trabalho JDBC/TrabJDBC/src/UsuariosDAO.java
+++ Trabalho JDBC/TrabJDBC/src/UsuariosDAO.java
@@ -1,6 +1,107 @@
 
 public class UsuariosDAO extends BasicoDAO{
-	
-	
 
+	/**
+	 * Inserir uma nova pessoa
+	 * @param pessoa
+	 */
+	public void insert(Usuario usuario) {
+
+		String sql = " insert into usuario(nome, sobrenome) values(?,?)";
+		
+		try (Connection conn = getConnection();
+			 PreparedStatement statement = conn.prepareStatement(sql)){
+			statement.setString(1, pessoa.getNome());
+			statement.setString(2, pessoa.getSobrenome());
+			statement.execute();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Deletar uma pessoa pelo id
+	 * @param pessoa
+	 */
+	public void delete(Pessoa pessoa) {
+		
+		String sql = " delete from pessoa where id = ?";
+		
+		try(Connection conn = getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql)) {
+			statement.setInt(1, pessoa.getId());
+			statement.execute();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Atualizar uma pessoa pelo id
+	 * @param pessoa
+	 */
+	public void update(Pessoa pessoa) {
+		String sql = " update pessoa set nome = ?, sobrenome = ? where id = ?";
+		
+		try (Connection conn = getConnection();
+			 PreparedStatement statement = conn.prepareStatement(sql)){
+			statement.setString(1, pessoa.getNome());
+			statement.setString(2, pessoa.getSobrenome());
+			statement.setInt(3, pessoa.getId());
+			statement.execute();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Obter uma pessoa pelo id
+	 * @param id
+	 * @return
+	 */
+	public Pessoa getById(int id) {
+		Pessoa pessoa = null;
+		String sql = " select id, nome, sobrenome from pessoa where id = ?";
+		
+		try(Connection conn = getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql)) {
+			statement.setInt(1, id);
+			ResultSet resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				pessoa = new Pessoa();
+				pessoa.setId(resultSet.getInt(1));
+				pessoa.setNome(resultSet.getString(2));
+				pessoa.setSobrenome(resultSet.getString(3));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return pessoa;
+	}
+	
+	/**
+	 * Obter todas as pessoas da tabel
+	 * @return
+	 */
+	public List<Pessoa> getAll(){
+		List<Pessoa> list = new ArrayList<>();
+		String sql = " select id, nome, sobrenome from pessoa order by nome";
+		
+		try(Connection conn = getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql)) {
+			ResultSet resultSet = statement.executeQuery();
+			while(resultSet.next()) {
+				Pessoa pessoa = new Pessoa();
+				pessoa.setId(resultSet.getInt(1));
+				pessoa.setNome(resultSet.getString(2));
+				pessoa.setSobrenome(resultSet.getString(3));
+				
+				list.add(pessoa);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
 }
