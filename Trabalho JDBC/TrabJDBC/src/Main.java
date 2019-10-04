3385137e75c380336aff6b50cfd1e2e043549da2
edufgar <35237258+edufgar@users.noreply.github.com>
2019-10-04 19:47:20 -0300

    Update Main.java

--- Trabalho JDBC/TrabJDBC/src/Main.java
+++ Trabalho JDBC/TrabJDBC/src/Main.java
@@ -1,46 +1,35 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		try {
-			//assim criar o banco colocar o nome no local 'nome_banco'
-			String stringConexao = "jdbc:mysql://localhost:3306/trabjdbc?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-			String usuario = "root";
-			String senha = "univille";
-			Connection conn = DriverManager.getConnection(stringConexao, 
-					usuario, 
-					senha);
-			
-			String sql = "select id,nome,sobrenome from pessoa";
-			
-			// String sql = "insert into pessoa values(?,?)";
-			
-			PreparedStatement p = conn.prepareStatement(sql);
-			
-			ResultSet r = p.executeQuery();
-			
-			while(r.next()) {
-				int id = r.getInt(1);
-				String nome = r.getString(2);
-				String sobrenome = r.getString(3);
-				System.out.println(id + " " + nome + " " + sobrenome);
-			}
-			
-			/*
-			p.setString(1, "Leanderso");
-			p.setString(2, "Andreé");
-			p.execute();
-			*/
-			
-		} catch(SQLException e) {
-			e.printStackTrace();
+		TarefasDAO tarefadao = new TarefasDAO();
+		
+		UsuariosDAO usuariodao = new UsuariosDAO();
+		
+		Tarefas t1 = new Tarefas();
+		t1.setDescricao("Teste");
+		t1.setDatalimite(new Date(21,12,2019));
+		
+		Usuarios u1 = new Usuarios();
+		u1.setUsuario("Fulano");
+		
+		usuariodao.insert(u1);
+		tarefadao.insert(t1);
+		
+		System.out.println(tarefadao.getByCodigotarefa(1));
+		
+		System.out.println(usuariodao.getByCodigousuario(1));
+		
+		for(Usuarios usuario : usuariodao.getAll()) {
+			System.out.println(usuario);
 		}
+		
+		for(Tarefas tarefa : tarefadao.getAll()) {
+			System.out.println(tarefa);
+		}
+		
 	}
 }
