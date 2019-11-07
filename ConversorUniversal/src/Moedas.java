d96a0d868944f5039cdf40b21dbbc78eeed838fe
edufgar <35237258+edufgar@users.noreply.github.com>
2019-11-07 11:23:06 -0300

    Update Moedas.java

--- ConversorUniversal/src/Moedas.java
+++ ConversorUniversal/src/Moedas.java
@@ -9,114 +9,139 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Moedas extends JPanel {
-	
-	private JTextField txtorigem;
-	private JTextField txtdestino;
-	
-	public Moedas() {
-			
-		setLayout();
-		
-	}
-	
-	private void setLayout() {
-		
-		// InÌcio Bloco JComboBox Moeda Origem
-		setLayout(null);
-				String nomemoedasorigem [] = {"Reais","Dolares","Euros"};
-						
-				JComboBox<String> comboBoxOrigem = new JComboBox<String>(nomemoedasorigem);
-				comboBoxOrigem.setBounds(80, 65, 100, 30);
-				// comboBoxOrigem.setSelectedIndex(-1);
-								
-				// Fim Bloco JComboBox Moeda Origem  
-				
-				// InÌcio Bloco JComboBox Moeda Destino
-						
-				String nomemoedasdestino [] = {"Reais","Dolares","Euros"};
-						
-				JComboBox<String> comboBoxDestino = new JComboBox<String>(nomemoedasdestino);
-				comboBoxDestino.setBounds(300, 65, 100, 30);
-				// comboBoxDestino.setSelectedIndex(-1);
-									
-				// Fim Bloco JComboBox Moeda Destino 
-				
-				JLabel lblorigem = new JLabel("De: ");
-				lblorigem.setForeground(Color.BLACK);
-				lblorigem.setBounds(35, 65, 80, 30);
-				
-				txtorigem = new JTextField(200);
-				txtorigem.setBounds(80, 125, 100, 30);
-				  
-				JLabel lbldestino = new JLabel("Para: ");
-				lbldestino.setForeground(Color.BLACK);
-				lbldestino.setBounds(245, 65, 80, 30);
-				
-				txtdestino = new JTextField(200);
-				txtdestino.setBounds(300, 125, 100, 30);
-				txtdestino.setEditable(false);
-							
-				JButton bttnconverter = new JButton("Converter!!");
-				bttnconverter.setBounds(300, 185, 100, 30);
-				
-				// AÁ„o do Bot„o Converter
-				
-				bttnconverter.addActionListener(new ActionListener() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						String moedade = (String) comboBoxOrigem.getSelectedItem();
-						String moedapara = (String) comboBoxDestino.getSelectedItem();
-						
-						double valorde = Double.parseDouble(txtorigem.getText());
-						double valorpara = 0;
-							
-						if(moedade.equals("Reais")&& moedapara.equals("Dolares")) {
-							valorpara = valorde / 3.99;
-						}
-						
-						if(moedade.equals("Dolares")&& moedapara.equals("Reais")) {
-							valorpara = valorde * 3.99;
-						}
-						
-						if(moedade.equals("Reais")&& moedapara.equals("Euros")) {
-							valorpara = valorde / 4.47;
-						}
-						
-						if(moedade.equals("Euros")&& moedapara.equals("Reais")) {
-							valorpara = valorde * 4.47;
-						}
-						
-						if(moedade.equals("Dolares")&& moedapara.equals("Euros")) {
-							valorpara = valorde / 1.12;
-						}
-						
-						if(moedade.equals("Euros")&& moedapara.equals("Dolares")) {
-							valorpara = valorde * 1.12;
-						}
-						
-						txtdestino.setText(String.format("%.2f", valorpara));		
-						
-					}
-				});
-				
-				// Fim AÁ„o Bot„o Converter
-				
-				// Adiciona os itens no frame
-				add(lblorigem);
-				add(lbldestino);
-				add(comboBoxOrigem);
-				add(comboBoxDestino);
-				add(txtorigem);
-				add(txtdestino);
-				add(bttnconverter);
-		
-	}
-	
-	public void LimparCampos() {
-		txtorigem.setText("");
-		txtdestino.setText("");
-	}
-	
-		
+
+    private JTextField txtorigem;
+    private JTextField txtdestino;
+    private JComboBox<String> comboBoxOrigem;
+    private JComboBox<String> comboBoxDestino;
+    private JLabel lblorigem;
+    private JLabel lbldestino;
+    private JButton bttninverter;
+    private JButton bttnconverter;
+    // private String nomemoedasorigem;
+    // private String nomemoedasdestino;
+
+    public Moedas() {
+
+        setLayout();
+
+    }
+
+    private void setLayout() {
+
+        // In√≠cio Bloco JComboBox Moeda Origem
+        setLayout(null);
+        String nomemoedasorigem [] = {"Reais","Dolares","Euros"};
+
+        comboBoxOrigem = new JComboBox<String>(nomemoedasorigem);
+        comboBoxOrigem.setBounds(80, 65, 100, 30);
+        // comboBoxOrigem.setSelectedIndex(-1);
+
+        // Fim Bloco JComboBox Moeda Origem
+
+        // In√≠cio Bloco JComboBox Moeda Destino
+
+        String nomemoedasdestino [] = {"Reais","Dolares","Euros"};
+
+        comboBoxDestino = new JComboBox<String>(nomemoedasdestino);
+        comboBoxDestino.setBounds(320, 65, 100, 30);
+        // comboBoxDestino.setSelectedIndex(-1);
+
+        // Fim Bloco JComboBox Moeda Destino
+
+        lblorigem = new JLabel("De: ");
+        lblorigem.setForeground(Color.BLACK);
+        lblorigem.setBounds(35, 65, 80, 30);
+
+        txtorigem = new JTextField(200);
+        txtorigem.setBounds(80, 125, 100, 30);
+
+        lbldestino = new JLabel("Para: ");
+        lbldestino.setForeground(Color.BLACK);
+        lbldestino.setBounds(265, 65, 80, 30);
+
+        txtdestino = new JTextField(200);
+        txtdestino.setBounds(320, 125, 100, 30);
+        txtdestino.setEditable(false);
+
+        bttninverter = new JButton("<->");
+        bttninverter.setBounds(195, 65, 55, 30);
+
+        // A√ß√£o do Bot√£o Inverter
+
+        bttninverter.addActionListener(e -> inverterOpcoes());
+
+        // Fim A√ß√£o Bot√£o Inverter
+
+        bttnconverter = new JButton("Converter!!");
+        bttnconverter.setBounds(320, 185, 100, 30);
+
+        // A√ß√£o do Bot√£o Converter
+
+        bttnconverter.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String moedade = (String) comboBoxOrigem.getSelectedItem();
+                String moedapara = (String) comboBoxDestino.getSelectedItem();
+
+                double valorde = Double.parseDouble(txtorigem.getText());
+                double valorpara = 0;
+
+                if(moedade.equals("Reais")&& moedapara.equals("Dolares")) {
+                    valorpara = valorde / 3.99;
+                }
+
+                if(moedade.equals("Dolares")&& moedapara.equals("Reais")) {
+                    valorpara = valorde * 3.99;
+                }
+
+                if(moedade.equals("Reais")&& moedapara.equals("Euros")) {
+                    valorpara = valorde / 4.47;
+                }
+
+                if(moedade.equals("Euros")&& moedapara.equals("Reais")) {
+                    valorpara = valorde * 4.47;
+                }
+
+                if(moedade.equals("Dolares")&& moedapara.equals("Euros")) {
+                    valorpara = valorde / 1.12;
+                }
+
+                if(moedade.equals("Euros")&& moedapara.equals("Dolares")) {
+                    valorpara = valorde * 1.12;
+                }
+
+                txtdestino.setText(String.format("%.2f", valorpara));
+
+            }
+        });
+
+        // Fim A√ß√£o Bot√£o Converter
+
+        // Adiciona os itens no frame
+        add(lblorigem);
+        add(lbldestino);
+        add(comboBoxOrigem);
+        add(comboBoxDestino);
+        add(txtorigem);
+        add(txtdestino);
+        add(bttninverter);
+        add(bttnconverter);
+
+    }
+
+    private void inverterOpcoes() {
+
+        int de = comboBoxOrigem.getSelectedIndex();
+        comboBoxOrigem.setSelectedIndex(comboBoxDestino.getSelectedIndex());
+        comboBoxDestino.setSelectedIndex(de);
+
+    }
+
+    public void LimparCampos() {
+        txtorigem.setText("");
+        txtdestino.setText("");
+    }
+
 }
